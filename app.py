0a7ecfce3d968d8f8576c63cf4a0574db53539bf
saad <sad1088gg@gmail.com>
2026-05-12 05:12:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
+secret = random.randint(1, 10)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html", message="ابدأ اللعبة 🎮")
+    msg = "خمن رقم بين 1 و 10"
+
+    if request.method == "POST":
+        guess = int(request.form["guess"])
+
+        if guess == secret:
+            msg = "🎉 صح! فزت"
+        elif guess > secret:
+            msg = "أصغر"
+        else:
+            msg = "أكبر"
+
+    return render_template("index.html", message=msg)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
